d2fb48f11dbb080518cd4140bb8705f6881f8c32
mohamed_magdy <mohamedmagdyabdelhamed99@gmail.com>
2023-06-10 10:57:08 +0200

    custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+const notFoundPage = () => {
+       return ( 
+              <div>
+                     <h1>Page is not found</h1>
+                     <p>Looks like this page is not exist!</p>
+              </div>
+        );
+}
+ 
+export default notFoundPage;
